22dc93ce0b5735c7c49cc5aea36387592c8bca1e
SonShant <140591333+SonShant@users.noreply.github.com>
2023-09-12 23:38:44 +0530

    ss
    ss

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app= express();
 mongoose.connect('mongodb://127.0.0.1:27017/myapp')
 .then(() => {
     console.log("Connected to Database");
-}) 
+})
 .catch(() => {
     console.log("Connection Failed!")
 });
